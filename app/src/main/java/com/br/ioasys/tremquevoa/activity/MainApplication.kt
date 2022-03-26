e32f680c1b49cf500af441441259c613925c2ff2
Pedro Bruno <pbruno1283@gmail.com>
2022-03-25 23:25:57 -0300

    Dependency Injection
    Criado os arquivos para trabalhar com injeção de dependências.

---
+++ app/src/main/java/com/br/ioasys/tremquevoa/activity/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.br.ioasys.tremquevoa.activity
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            modules(
+
+            ).androidContext(applicationContext)
+        }
+    }
+
+}
