d64e37d6266e04fe17814f52844c06a2525cc0cc
Pedro Bruno <pbruno1283@gmail.com>
2022-04-19 21:45:51 -0300

    LayoutPopUp
    Criado o layout para o pop up

--- app/src/main/java/com/br/ioasys/tremquevoa/activity/HomeActivity.kt
+++ app/src/main/java/com/br/ioasys/tremquevoa/activity/HomeActivity.kt
@@ -1,8 +1,17 @@
 package com.br.ioasys.tremquevoa.activity
 
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -11,10 +20,13 @@ import com.br.ioasys.tremquevoa.databinding.ActivityHomeBinding
 import com.br.ioasys.tremquevoa.presentation.ui.fragments.HomeFragment
 import com.br.ioasys.tremquevoa.presentation.ui.fragments.PerfilUserFragment
 import com.br.ioasys.tremquevoa.presentation.ui.fragments.RegisterEventFragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var materialAlertDialogBuilder: MaterialAlertDialogBuilder
+    private lateinit var customAlertDialogView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +38,8 @@ class HomeActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         binding.menuBottomNavigation.setupWithNavController(navController)
+        customAlertDialogView = LayoutInflater.from(this)
+            .inflate(R.layout.pop_up_home, null, false)
 
         showDialog()
         replaceFragment(HomeFragment())
@@ -64,9 +78,21 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun showDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setView(R.layout.pop_up_home)
-        builder.create()
-        builder.show()
+        val dialog =
+            MaterialAlertDialogBuilder(
+                this,
+                R.style.MaterialAlertDialog_Rounded
+            )
+        dialog.setView(customAlertDialogView)
+        val text =
+            customAlertDialogView.findViewById(R.id.textViewMotivationalMessage) as AppCompatTextView
+        dialog.setPositiveButtonIcon(ContextCompat.getDrawable(this,R.drawable.ic_error))
+//        dialog.setPositiveButton(
+//            "Confirmar",
+//            DialogInterface.OnClickListener { dialogInterface, i ->
+//                Unit
+//            })
+        dialog.create()
+        dialog.show()
     }
 }
