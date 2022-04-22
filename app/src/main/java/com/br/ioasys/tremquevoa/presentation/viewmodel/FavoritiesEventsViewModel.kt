c66cc5ad65bc4bb656bc1774845d20f894deca4f
Priscilla Souza <priscillasilva-24@hotmail.com>
2022-04-22 18:35:42 -0300

    Criando estrutura para favoritar os eventos

---
+++ app/src/main/java/com/br/ioasys/tremquevoa/presentation/viewmodel/FavoritiesEventsViewModel.kt
@@ -0,0 +1,42 @@
+package com.br.ioasys.tremquevoa.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.br.ioasys.tremquevoa.domain.model.Event
+import com.br.ioasys.tremquevoa.domain.model.EventLists
+import com.br.ioasys.tremquevoa.domain.usecase.GetAllEventsUseCase
+import com.br.ioasys.tremquevoa.domain.usecase.RegisterParticipateEventsUseCase
+import com.br.ioasys.tremquevoa.util.ViewState
+import com.br.ioasys.tremquevoa.util.postError
+import com.br.ioasys.tremquevoa.util.postLoading
+import com.br.ioasys.tremquevoa.util.postSuccess
+
+class FavoritiesEventsViewModel(
+    private val getAllEventsUseCase: GetAllEventsUseCase
+) : ViewModel() {
+
+    private var _events = MutableLiveData<ViewState<List<Event>>>()
+    var events: LiveData<ViewState<List<Event>>> = _events
+
+    init {
+        getEventsFavorities()
+    }
+
+    private fun getEventsFavorities() {
+        _events.postLoading()
+
+        getAllEventsUseCase(
+            params = Unit,
+            onSuccess = {
+                _events.postSuccess(it.filter {
+                    it.isFavorite
+                })
+            },
+            onError = {
+                it.printStackTrace()
+                _events.postError(it)
+            }
+        )
+    }
+}
